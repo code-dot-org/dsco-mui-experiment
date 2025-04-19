f2d65930af726aba968bd5c06183e573df8327a6
Seth Nickell <snickell@gmail.com>
2025-04-19 04:34:43 -1000

    remove unnecessary size attr

--- src/checkbox/MuiCheckbox.tsx
+++ src/checkbox/MuiCheckbox.tsx
@@ -2,7 +2,6 @@ import { Checkbox, CheckboxProps, FormControlLabel } from '@mui/material';
 
 export interface MuiCheckboxProps extends CheckboxProps {
   label: string;
-  size?: 'small' | 'medium' | 'large';
 }
 
 export const MuiCheckbox = ({ label, size = 'medium', ...rest }: MuiCheckboxProps) => (
