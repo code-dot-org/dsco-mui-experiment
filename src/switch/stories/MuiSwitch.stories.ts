398808d3eccfe60fb5c79914a4fa37794dc37671
Seth Nickell <snickell@gmail.com>
2025-04-19 04:35:33 -1000

    MuiSwitch (implements Toggle)

---
+++ src/switch/stories/MuiSwitch.stories.ts
@@ -0,0 +1,12 @@
+import { MuiSwitch } from '../MuiSwitch'
+
+export default {
+  component: MuiSwitch,
+  title: 'DesignSystem/MUI/Switch',
+}
+
+export const Default = {
+  args: { 
+    label: 'Switch Label',
+  },
+}
